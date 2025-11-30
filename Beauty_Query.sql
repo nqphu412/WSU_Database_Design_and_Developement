5ad41717639cbadeb20ac511c89704299aef5fb7
Elio Nguyen <quangphu.work412@gmail.com>
2025-12-01 03:49:52 +1100

    Refactor SQL queries to use Beauty_ prefix
    Updated SQL queries to use 'Beauty_' prefix for tables and adjusted joins accordingly.

--- Beauty_Query.sql
+++ Beauty_Query.sql
@@ -1,14 +1,12 @@
 ----------------------
--- Question 3(i) - Create the database tables in SQL with relevant constraints
--- Fill the tables with sufficient data
--- List the content of your tables with screenshots.
+-- Question 3(i) - Show table result
 ----------------------
-SELECT * FROM Staff;
-SELECT * FROM Client;
-SELECT * FROM Service;
-SELECT * FROM TimeService;
-SELECT * FROM Booking;
-SELECT * FROM Therapy;
+SELECT * FROM Beauty_Staff;
+SELECT * FROM Beauty_Client;
+SELECT * FROM Beauty_Service;
+SELECT * FROM Beauty_TimeService;
+SELECT * FROM Beauty_Booking;
+SELECT * FROM Beauty_Therapy;
 
 ----------------------
 -- Question 3(ii) - SQL command to fulfill following tasks with screenshot
@@ -16,9 +14,9 @@ SELECT * FROM Therapy;
 -- The list should be sorted alphabetically in the service names.
 ----------------------
 SELECT serviceName, (st.staffFname + ' ' + st.staffLname) AS staffFullName
-FROM Service s
-LEFT JOIN TimeService ts ON s.serviceID = ts.serviceID
-LEFT JOIN Staff st ON st.staffID = ts.staffID
+FROM Beauty_Service s
+LEFT JOIN Beauty_TimeService ts ON s.serviceID = ts.serviceID
+LEFT JOIN Beauty_Staff st ON st.staffID = ts.qualifiedStaffID
 WHERE type = 'Time-based'
 ORDER BY serviceName ASC;
 
@@ -28,26 +26,21 @@ ORDER BY serviceName ASC;
 -- at least one booking on that day. Don't repeat the names in the list.
 ----------------------
 SELECT DISTINCT (s.staffFname + ' ' + s.staffLname) AS staffFullName
-FROM Booking b 
-JOIN Therapy t ON b.bookingID = t.bookingID
-JOIN Staff s ON t.performStaffID = s.staffID
+FROM Beauty_Booking b 
+JOIN Beauty_Therapy t ON b.bookingID = t.bookingID
+JOIN Beauty_Staff s ON t.performStaffID = s.staffID
 WHERE bookedDate = '2024-11-11';
-
-SELECT (s.staffFname + ' ' + s.staffLname) AS staffFullName
-FROM Booking b 
-JOIN Therapy t ON b.bookingID = t.bookingID
-JOIN Staff s ON t.performStaffID = s.staffID
-WHERE b.bookedDate = '2024-11-11'
-GROUP BY (s.staffFname + ' ' + s.staffLname) ;
 
 ----------------------
 -- Question 3(ii) - SQL command to fulfill following tasks with screenshot
 -- c. List the names of all the clients along with the corresponding total number of bookings.
 ----------------------
-SELECT (clientFname + ' ' + clientLname) AS clientName, COUNT(b.clientID)
-FROM Booking b
-JOIN Client c ON b.clientID = c.clientID
-GROUP BY b.clientID;
+SELECT (clientFname + ' ' + clientLname) AS clientName, 
+	COUNT(clientFname + ' ' + clientLname)
+FROM Beauty_Booking b
+LEFT JOIN Beauty_Client c ON c.clientID = b.clientID
+WHERE b.clientID != 'Walk-in' -- Excluding Walk-in clients
+GROUP BY (clientFname + ' ' + clientLname);
 
 ----------------------
 -- Question 3(ii) - SQL command to fulfill following tasks with screenshot
@@ -55,22 +48,84 @@ GROUP BY b.clientID;
 -- Sort the output according to their staff name, service type (timed or itemised), the name of the service.
 ----------------------
 SELECT 
-    (st.staffFname + ' ' + st.staffLname) AS staffFullName,
-    s.serviceName,
-    s.type AS serviceType
+    DISTINCT staffFullName, type AS serviceType, serviceName
 FROM 
-    Staff st
+    (SELECT staffID, (staffFName + ' ' + staffLName) AS staffFullName
+	FROM Beauty_Staff
+	WHERE position = 'Beauty Therapist') AS st -- Only Beauty Therapist can perform services
 LEFT JOIN 
-    TimeService ts ON st.staffID = ts.qualifiedStaffID
+    Beauty_TimeService ts ON st.staffID = ts.qualifiedStaffID
 LEFT JOIN 
-    Service s ON ts.serviceID = s.serviceID OR s.type = 'Item-based'
+    Beauty_Service s ON s.type = 'Item-based' OR ts.serviceID = s.serviceID 
 ORDER BY 
-    (st.staffFname + ' ' + st.staffLname),
-    s.type,
-    s.serviceName;
+    st.staffFullName, s.type, s.serviceName;
 
-----------------------
+---------------------- 
 -- Question 3(ii) - SQL command to fulfill following tasks with screenshot
 -- e. For each timed service, list the names of the therapists whose hourly rates are the cheapest,
 -- along with their actual hourly rates.
 ----------------------
+SELECT serviceName, staffFName + ' ' + staffLName AS staffFullName, cheapestPrice
+FROM Beauty_TimeService ts -- This only includes timed service
+JOIN 
+	Beauty_Service s ON ts.serviceID = s.serviceID
+LEFT JOIN 
+	Beauty_Staff st ON st.staffID = ts.qualifiedStaffID
+LEFT JOIN ( -- This is a table that includes only services with their corresponding cheapest price
+	SELECT ts2.serviceID, MIN(ts2.timePrice) AS cheapestPrice
+	FROM Beauty_TimeService ts2
+	GROUP BY ts2.serviceID
+	) AS minPrice ON minPrice.serviceID = ts.serviceID
+WHERE timePrice = cheapestPrice;
+
+-- Extra: Calculate total price for each therapy
+-- Base for later use
+SELECT b.bookingID, type, duration, quantityItemUsed, itemPrice, timePrice,
+	CASE 
+        WHEN type = 'Item-based' THEN quantityItemUsed*itemPrice
+        WHEN type = 'Time-based' THEN duration*timePrice/60
+    END AS TotalCost
+FROM Beauty_Booking b
+JOIN Beauty_Therapy t ON b.bookingID = t.bookingID
+LEFT JOIN Beauty_Service s ON b.serviceID = s.serviceID
+LEFT JOIN Beauty_TimeService ts ON ts.qualifiedStaffID = t.performStaffID AND ts.serviceID = b.serviceID;
+
+---------------------- 
+-- Question 3(iii) - Indicate a scenario that business current database integrity is not ensured
+-- SQL code and screenshot
+----------------------
+-- Scenario 1: Assigning �not� qualified therapist to perform a time-based service
+INSERT INTO Beauty_Booking VALUES
+('B027', 'ST004', 'C002', 'SV004', '2024-11-15', '10:10:00', '11:50:00');
+
+INSERT INTO Beauty_Therapy VALUES 
+('B027', 'ST007', 3);
+
+/*
+DELETE FROM Beauty_Booking WHERE bookingID = 'B027';
+DELETE FROM Beauty_Therapy WHERE bookingID = 'B027';
+*/
+
+SELECT b.bookingID, type, duration, quantityItemUsed, itemPrice, timePrice,
+	CASE 
+        WHEN type = 'Item-based' THEN quantityItemUsed*itemPrice
+        WHEN type = 'Time-based' THEN duration*timePrice/60
+    END AS TotalCost
+FROM Beauty_Booking b
+JOIN Beauty_Therapy t ON b.bookingID = t.bookingID
+LEFT JOIN Beauty_Service s ON b.serviceID = s.serviceID
+LEFT JOIN Beauty_TimeService ts ON ts.qualifiedStaffID = t.performStaffID AND ts.serviceID = b.serviceID
+WHERE b.bookingID = 'B027';
+
+-- Scenario 2: Booking staff allow client to book overlapping timeslot
+INSERT INTO Beauty_Booking VALUES
+('B027', 'ST004', 'C002', 'SV004', '2024-11-15', '10:10:00', '11:50:00'),
+('B028', 'ST004', 'C002', 'SV001', '2024-11-15', '10:30:00', '11:40:00');
+
+/*
+DELETE FROM Beauty_Booking WHERE bookingID = 'B027';
+DELETE FROM Beauty_Booking WHERE bookingID = 'B028';
+*/
+
+SELECT * FROM Beauty_Booking
+WHERE bookedDate = '2024-11-15';
